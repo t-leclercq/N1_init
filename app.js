2334809be0dbdb22f1ee1539dadc5eda5c45c6db
Thomas Leclercq <11087275+t-leclercq@users.noreply.github.com>
2020-11-08 19:58:51 +0100

    Login routes + middlewares ⚙✔

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
---
+++ controllers/user.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            username: req.body.username,
+            password: hash
+        });
+        user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({ username: req.body.username })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if (!valid) {
+                return res.status(401).json({ error: 'Mot de passe incorrect !'});
+            }
+            res.status(200).json({
+                username: user.username,
+                token: jwt.sign(
+                    { username: user.username },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24h' }
+                )
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
